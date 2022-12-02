aba2250d4bc5620206a276db620f30ca8a629ecf
seokjin8678 <rnrn8678@naver.com>
2022-12-03 03:17:36 +0900

    feat: User 엔티티 테이블 명 변경, 닉네임 필드 추가

--- src/main/java/com/seokjin/travelguide/domain/User.java
+++ src/main/java/com/seokjin/travelguide/domain/User.java
@@ -1,17 +1,35 @@
 package com.seokjin.travelguide.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
 }
