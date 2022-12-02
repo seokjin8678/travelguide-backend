eb1b19cb20ac02be108b147e141c50bcff690189
seokjin8678 <rnrn8678@naver.com>
2022-12-03 03:50:07 +0900

    feat: 회원가입 Request Dto 추가

---
+++ src/main/java/com/seokjin/travelguide/dto/request/SignUpRequest.java
@@ -0,0 +1,41 @@
+package com.seokjin.travelguide.dto.request;
+
+import com.seokjin.travelguide.exception.InvalidRequestException;
+import com.seokjin.travelguide.exception.Validation;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SignUpRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String confirmPassword;
+
+    @NotBlank
+    private String nickname;
+
+    public SignUpRequest(String email, String password, String confirmPassword, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.nickname = nickname;
+    }
+
+    public void validate() {
+        if (!password.equals(confirmPassword)) {
+            throw new InvalidRequestException(
+                    Validation.of("password", "비밀번호와 확인 비밀번호가 맞지 않습니다."),
+                    Validation.of("confirmPassword", "비밀번호와 확인 비밀번호가 맞지 않습니다."));
+        }
+    }
+}
