fbf57e540fa271cf0ac2f2fd4c31826f6273652f
seokjin8678 <rnrn8678@naver.com>
2022-12-04 02:03:21 +0900

    feat: 테스트용 임시 api 추가

--- src/main/java/com/seokjin/travelguide/controller/HomeController.java
+++ src/main/java/com/seokjin/travelguide/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.seokjin.travelguide.controller;
 
+import com.seokjin.travelguide.dto.response.Response;
+import com.seokjin.travelguide.dto.response.SuccessResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,14 @@ public class HomeController {
     public ResponseEntity<String> home() {
         return ResponseEntity.ok()
                 .body("Hello World!");
+    }
+
+    @PostMapping("/api/v1/test")
+    public ResponseEntity<Response> test() {
+        return ResponseEntity.ok()
+                .body(SuccessResponse.builder()
+                        .code("200")
+                        .message("Hello World!")
+                        .build());
     }
 }
