2a0cd01e2e9bfe232cc1a4aeed66263fa0d7d5ad
seokjin8678 <rnrn8678@naver.com>
2022-12-05 13:40:04 +0900

    feat: ip 주소 properties로 이동

--- src/main/java/com/seokjin/travelguide/config/CorsConfig.java
+++ src/main/java/com/seokjin/travelguide/config/CorsConfig.java
@@ -1,18 +1,21 @@
 package com.seokjin.travelguide.config;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
+@PropertySource("classpath:ip.properties")
 public class CorsConfig {
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource(@Value("${frontend-ip-address}") String ipAddress) {
         return request -> {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+            configuration.setAllowedOrigins(List.of(ipAddress));
             configuration.setAllowedHeaders(List.of("*"));
             configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             configuration.setMaxAge(3600L);
