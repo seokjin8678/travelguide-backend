bfa6719f095e4904af6d143401352f1aeee694f8
seokjin8678 <rnrn8678@naver.com>
2022-12-06 18:56:30 +0900

    refactor: 보안 예외 핸들링 SecurityConfig -> ExceptionController로 이동

--- src/main/java/com/seokjin/travelguide/config/SecurityConfig.java
+++ src/main/java/com/seokjin/travelguide/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.seokjin.travelguide.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.seokjin.travelguide.dto.response.ErrorResponse;
 import com.seokjin.travelguide.service.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +20,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig {
 
-    private final ObjectMapper objectMapper;
     private final JwtTokenProvider jwtTokenProvider;
     private final CorsConfigurationSource corsConfigurationSource;
 
@@ -41,26 +36,6 @@ public class SecurityConfig {
                 .and()
                 .addFilterBefore(new JwtAuthorizationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling()
-                .authenticationEntryPoint((request, response, authException) -> {
-                    log.info("{}에 대한 요청이 실패했습니다. (권한 없음) IP={}", request.getRequestURI(), request.getRemoteAddr());
-                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                    objectMapper.writeValue(
-                            response.getOutputStream(),
-                            ErrorResponse.UNAUTHORIZED
-                    );
-                })
-                .accessDeniedHandler((request, response, accessDeniedException) -> {
-                    log.info("{}에 대한 요청이 실패했습니다. (접근 제한) IP={}", request.getRequestURI(), request.getRemoteAddr());
-                    response.setStatus(HttpStatus.FORBIDDEN.value());
-                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                    objectMapper.writeValue(
-                            response.getOutputStream(),
-                            ErrorResponse.FORBIDDEN
-                    );
-                })
-                .and()
                 .authorizeRequests()
                 .antMatchers("/api/v1/auth/signin").permitAll()
                 .antMatchers("/api/v1/auth/signup").permitAll()
